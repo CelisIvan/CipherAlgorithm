517430b43c905c0bb427eda50a4d0ab1d9940266
Sandra <sandraromanrivera@hotmail.com>
2021-03-17 11:35:51 -0600

    switched whiles for fors

--- TBD.cpp
+++ TBD.cpp
@@ -3,20 +3,17 @@
 using namespace std;
 
 //function for rows shifting
-void shift( int  mat[32][32]){
-    int pos=0, temp, posTemp;
-    while(pos<32){
-        posTemp=pos;
-        while(posTemp){
+void shift(int  mat[32][32]){
+    int temp, posTemp;
+    for(int pos = 0; pos < 32; pos++){
+        for(posTemp = pos; posTemp > 0; posTemp--){
             // shift a la izquierda
             temp = mat[pos][0];
-            for(int i=0;i<32;i++)
+            for(int i = 0; i < 32; i++)
                mat[pos][i] = mat[pos][i+1];
 
             mat[pos][31] = temp;
-            posTemp--;
         }
-        pos++;
     }
 }
 
@@ -32,14 +29,12 @@ int main(){
 
     cout << "Please enter the message to cipher\n";
     getline(cin,message);
-    int pos = 0;
-    while(pos<message.length()){
+    for(int pos = 0; pos < message.length(); pos++){
         for(int i=0;i<32;i++){
             for(int j=0;j<32;j++){
                 block[i][j]=message[pos];
             }
         }
-        pos++;
     }
 
     return 0;
